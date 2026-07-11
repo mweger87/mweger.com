70c4a36401b01752af2e2239c99c0f88d2c9225b
mweger <weger.max55@gmail.com>
2026-07-11 14:43:12 -0700

    First deployment of the website

--- app.py
+++ app.py
@@ -2,71 +2,127 @@ from flask import Flask
 from flask import render_template
 from flask import url_for
 from flask import session, redirect, request, jsonify
-from flask_sqlalchemy import SQLAlchemy
 from werkzeug.security import generate_password_hash, check_password_hash
 import os
 from dotenv import load_dotenv
+from flask_mysqldb import MySQL
+import MySQLdb.cursors
+import re 
 
 load_dotenv()
 
 app = Flask(__name__)
-app.secret_key = "really-random"
-app.config["SQLALCHEMY_DATABASE_URI"] = "DATABASE_URL"
-db = SQLAlchemy(app)
+app.secret_key = os.environ['SECRET_KEY']
 
 
-class User(db.Model):
-    id = db.Column(db.Integer, primary_key=True)
-    username = db.Column(db.String(80), unique=True, nullable=False)
-    password_hash = db.Column(db.String(200), nullable=False)
+app.config['MYSQL_HOST'] = os.environ['MYSQL_HOST'] 
+app.config['MYSQL_USER'] = os.environ['MYSQL_USER']
+app.config['MYSQL_PASSWORD'] = os.environ['MYSQL_PASSWORD']
+app.config['MYSQL_DB'] = os.environ['MYSQL_DB']
 
-with app.app_context():
-    db.create_all()
-
+mysql = MySQL(app)
 
 
 @app.route("/")
 @app.route("/home")
-def home(name=None):
-    return render_template('index.html', user=session.get("user"))
-
+def home():
+    return render_template('index.html', user=session.get("username"))
 
 @app.route("/about")
 def about():
     return render_template('about.html')
 
-@app.route("/login", methods=["GET", "POST"])
+@app.route("/projects")
+def projects():
+    return render_template('projects.html')
+
+@app.route("/login", methods=['GET', 'POST'])
 def login():
-    if request.method == "POST":
+    if request.method == 'POST':
         data = request.get_json()
-        username = data["username"]
-        password = data["password"]
-        user = User.query.filter_by(username=username).first()
-        if user and check_password_hash(user.password_hash, password):
-            session["user"] = username
+        username = data.get('username')
+        password = data.get('password')
+        cursor = mysql.connection.cursor(MySQLdb.cursors.DictCursor)
+        cursor.execute(
+            'SELECT * FROM Users where username = %s', (username, )
+        )
+        account = cursor.fetchone()
+        if account and check_password_hash(account['password'], password):
+            session['loggedin'] = True
+            session['id'] = account['id']
+            session['username'] = account['username']
+            msg = 'Log in success'
             return jsonify({"success": True})
         else:
-            return jsonify({"success": False})
+            msg = 'Log in failed'
+            return jsonify({"error": "Log in failed"}), 401
     return render_template('login.html')
 
-@app.route("/login/register", methods=["GET", "POST"])
+@app.route("/register", methods=["GET", "POST"])
 def register():
-    if request.method == "POST":
+    msg = ''
+    
+    if request.method == 'POST':
         data = request.get_json()
-        username = data["username"]
-        password = data["password"]
+        username = data.get('username')
+        password = data.get('password')
+        email = data.get('email')
 
-        if User.query.filter_by(username=username).first():
-            return jsonify({"error": "username already taken"}), 400
+        hashed_password = generate_password_hash(password)
+
+        cursor = mysql.connection.cursor(MySQLdb.cursors.DictCursor)
+
+        cursor.execute(
+            'SELECT * FROM Users WHERE username = %s',
+            (username,)
+        )
+        account_username = cursor.fetchone()
+
+        cursor.execute(
+            'SELECT * FROM Users WHERE email = %s',
+            (email, )
+        )
+        account_email = cursor.fetchone()
+
+        if account_username:
+            msg = 'Username already exists'
+        elif account_email:
+            msg = 'Email already used'
+        elif not re.match(
+                r'[^@]+@[^@]+\.[^@]+',
+                email):
+            msg = 'Inavlid email'
+        elif not re.match(
+                r'[A-Za-z0-9]+',
+                username):
+            msg = 'Username must contain only letters and numbers'
         
-        password_hash = generate_password_hash(password)
-        new_user = User(username=username, password_hash=password_hash)
-        db.session.add(new_user)
-        db.session.commit()
-        return jsonify({"success": True})
-    return render_template('register.html')
+        else:
+            cursor.execute(
+                '''INSERT INTO Users (username, email, password, role)
+                VALUES ( %s, %s, %s, %s)''',
+                (
+                    username,
+                    email,
+                    hashed_password,
+                    'user'
+                )
+            )
 
-@app.route("/logout/api")
+            mysql.connection.commit()
+            msg = 'Regsiter success'
+    
+    elif request.method == 'POST':
+        msg = 'Fill out the form'
+
+    return render_template(
+        'register.html',
+        msg=msg
+    )
+    
+@app.route("/logout")
 def logout():
-    session.pop("user", None)
+    session.pop('loggedin', None)
+    session.pop('id', None)
+    session.pop('username', None)
     return redirect(url_for("home"))
